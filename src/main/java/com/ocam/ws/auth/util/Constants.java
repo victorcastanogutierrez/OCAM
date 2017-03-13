cc0524aa1ae41abce066f7bf3bd7118778f605a0
Victor Castaño <victorcastano@MacBook-Pro-de-Victor.local>
2017-03-13 20:07:44 +0100

    Cambiada constante de cabecera en la que viene proporcionado el token

--- src/main/java/com/ocam/ws/auth/util/Constants.java
+++ src/main/java/com/ocam/ws/auth/util/Constants.java
@@ -2,7 +2,7 @@ package com.ocam.ws.auth.util;
 
 public class Constants {
 
-	public static final String JWT_TOKEN_HEADER_PARAM = "ocam-token";
+	public static final String JWT_TOKEN_HEADER_PARAM = "authorization";
 	public static final String FORM_BASED_LOGIN_ENTRY_POINT = "/api/auth/login";
 	public static final String TOKEN_BASED_AUTH_ENTRY_POINT = "/api/**";
 	public static final String TOKEN_REFRESH_ENTRY_POINT = "/api/auth/token";
