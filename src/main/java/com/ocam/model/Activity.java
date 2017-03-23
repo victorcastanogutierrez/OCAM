3a2ca167a297b34f907269ff60f66311cebd1cbd
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2017-03-24 00:18:30 +0100

    Adaptación de campo Lob para Postgres

--- src/main/java/com/ocam/model/Activity.java
+++ src/main/java/com/ocam/model/Activity.java
@@ -10,6 +10,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -46,6 +47,7 @@ public class Activity extends BaseEntity {
 
 	@Column(name = "TRACK", columnDefinition = "LONGBLOB")
 	@NotNull
+	@Lob
 	private String track;
 
 	@Enumerated(EnumType.STRING)
