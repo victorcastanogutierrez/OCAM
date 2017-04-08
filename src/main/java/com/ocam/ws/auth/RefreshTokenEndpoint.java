ae01741764a7f385a9932d76b47a0bfd1874809e
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2017-04-08 13:30:43 +0200

    Añadida información de login en la API de refreshToken

--- src/main/java/com/ocam/ws/auth/RefreshTokenEndpoint.java
+++ src/main/java/com/ocam/ws/auth/RefreshTokenEndpoint.java
@@ -1,6 +1,8 @@
 package com.ocam.ws.auth;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -8,19 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ocam.model.Hiker;
 import com.ocam.repository.HikerRepository;
 import com.ocam.ws.auth.config.ConfigurationSettings;
 import com.ocam.ws.auth.exception.InvalidJwtToken;
-import com.ocam.ws.auth.jwt.JwtToken;
+import com.ocam.ws.auth.jwt.AccessJwtToken;
 import com.ocam.ws.auth.jwt.JwtTokenFactory;
 import com.ocam.ws.auth.model.RawAccessJwtToken;
 import com.ocam.ws.auth.model.RefreshToken;
@@ -37,7 +40,7 @@ public class RefreshTokenEndpoint {
 
 	@RequestMapping(value = "/auth/token", method = RequestMethod.GET,
 			produces = { MediaType.APPLICATION_JSON_VALUE })
-	public @ResponseBody JwtToken refreshToken(HttpServletRequest request,
+	public ResponseEntity<?> refreshToken(HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException {
 
 		String tokenPayload = request
@@ -62,6 +65,15 @@ public class RefreshTokenEndpoint {
 		UserContext userContext = UserContext.create(hiker.getLogin(),
 				hiker.getEmail());
 
-		return tokenFactory.createAccessJwtToken(userContext);
+		AccessJwtToken newToken = tokenFactory
+				.createAccessJwtToken(userContext);
+
+		Map<String, String> responseMap = new HashMap<String, String>();
+		responseMap.put("token", newToken.getToken());
+		responseMap.put("refreshToken", tokenPayload);
+		responseMap.put("email", hiker.getEmail());
+		responseMap.put("username", hiker.getLogin());
+
+		return new ResponseEntity<Object>(responseMap, HttpStatus.OK);
 	}
 }
